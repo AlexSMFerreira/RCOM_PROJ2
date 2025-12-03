6ceeee90faa12e2e92f068189e4a10fcbb9436c9
AlexSMFerreira <alexandre.smferreira@gmail.com>
2025-12-03 15:57:27 +0000

    first attempt at automation

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # RCOM_PROJ2
 An ftp client for the computer networks class.
+
+## Hardware Setup
+TUXY3 E1 -> ether13 (Switch)
+TUXY4 E1 -> ether14 (Switch)
+TUXY4 E2 -> ether24 (Switch)
+TUXY2 E1 -> ether12 (Switch)
+TUXY2 S0 -> Cross PC
+Cross Cons -> SW Cons
+
+## Software Setup
+Run `./setup.sh`
---
+++ setup.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# This script configures the network automatically.
+# It should be run with superuser privileges on any TUX.
+
+password="alanturing"
+
+# Set Y as your group number.
+Y=10
+
+#tux_name without tux number (e.g., netedu@tux102 for TUXY2 of group 10)
+tux_name="netedu@tux$Y" 
+
+# IPs
+ip_tuxy2_ife1="172.16.${Y}1.1"
+ip_tuxy3_ife1="172.16.${Y}0.1"
+ip_tuxy4_ife1="172.16.${Y}0.254"
+ip_tuxy4_ife2="172.16.${Y}1.253"
+
+# SSH into TUXY2
+sshpass -p "$password" ssh netedu@tux${Y}2
+# Set the static IP address of if_e1 to 172.16.Y1.1
+ifconfig if_e1 $ip_tuxy2_ife1/24
+
+# Setup the switch
+stty -F /dev/ttyS0 115200 cs8 -cstopb -parenb -echo
+echo -e "/system reset-configuration" > /dev/ttyS0
+sleep 1  # wait for prompt to appear
+echo -e "y\r" > /dev/ttyUSB0
+sleep 5  # wait for the system to reset
+
+# Add bridges on the switch
+echo -e "/interface bridge add name=bridge${Y}0\r" > /dev/ttyS0
+echo -e "/interface bridge add name=bridge${Y}1\r" > /dev/ttyS0
+
+# Remove ports from default bridge
+echo -e "/interface bridge port remove [find interface=ether12]\r" > /dev/ttyS0
+echo -e "/interface bridge port remove [find interface=ether13]\r" > /dev/ttyS0
+echo -e "/interface bridge port remove [find interface=ether14]\r" > /dev/ttyS0
+echo -e "/interface bridge port remove [find interface=ether24]\r" > /dev/ttyS0
+
+# Add ports to the new bridges
+echo -e "/interface bridge port add bridge=bridge${Y}1 interface=ether12\r" > /dev/ttyS0
+echo -e "/interface bridge port add bridge=bridge${Y}0 interface=ether13\r" > /dev/ttyS0
+echo -e "/interface bridge port add bridge=bridge${Y}0 interface=ether14\r" > /dev/ttyS0
+echo -e "/interface bridge port add bridge=bridge${Y}1 interface=ether24\r" > /dev/ttyS0
+
+# Exit TUXY2
+exit
+
+# SSH into TUXY3
+sshpass -p "$password" ssh netedu@tux${Y}3
+
+# Set the static IP address of if_e1 to 172.16.Y0.1
+ifconfig if_e1 $ip_tuxy3_ife1/24
+
+# Exit TUXY3
+exit
+
+# SSH into TUXY4 (Router)
+sshpass -p "$password" ssh netedu@tux${Y}4
+# Set the static IP address of if_e1 to 172.16.Y0.254
+ifconfig if_e1 $ip_tuxy4_ife1/24
+# Set the static IP address of if_e2 to 172.16.Y1.253
+ifconfig if_e2 $ip_tuxy4_ife2/24
+
+# Enable IP forwarding
+sysctl net.ipv4.ip_forward=1
+# Disable ICMP echo ignore broadcast
+sysctl net.ipv4.icmp_echo_ignore_broadcasts=0
+
+# Exit TUXY4
+exit
